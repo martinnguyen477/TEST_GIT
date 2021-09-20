3d2074e1bc4485ff3b3e90851b8b41b330cb606b
nqcuong720 <nqcuong720@gmail.com>
2021-09-21 00:16:37 +0700

    first commit

---
+++ CRUD_KENDOUI/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using CRUD_KENDOUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_KENDOUI.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerRepository customerRepository = new CustomerRepository();
+
+        // GET: CustomerController
+        public JsonResult GetCustomer()
+        {
+            var cus = customerRepository.GetAllCustomer();
+            var result = new
+            {
+                Items = cus,
+                TotalCount = cus.Count
+            };
+            return new JsonResult(result);
+
+        }
+    }
+}
---
+++ CRUD_KENDOUI/Models/CustomerRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_KENDOUI.Models
+{
+    public class CustomerRepository
+    {
+        public List<CustomerModel> GetAllCustomer()
+        {
+            var customer = new List<CustomerModel>()
+            {
+                new CustomerModel{ Id = 1, Name = "Nguyễn Quốc Cường", Address = "Thôn 8 xã Earal huyện Eahleo tỉnh Đak Lak", Phone = "0377077630"},
+                new CustomerModel{ Id = 2, Name = "Nguyễn Quốc Cường", Address = "Thôn 8 xã Earal huyện Eahleo tỉnh Đak Lak", Phone = "0377077630"},
+                new CustomerModel{ Id = 3, Name = "Nguyễn Quốc Cường", Address = "Thôn 8 xã Earal huyện Eahleo tỉnh Đak Lak", Phone = "0377077630"},
+                new CustomerModel{ Id = 4, Name = "Nguyễn Quốc Cường", Address = "Thôn 8 xã Earal huyện Eahleo tỉnh Đak Lak", Phone = "0377077630"},
+                new CustomerModel{ Id = 5, Name = "Nguyễn Quốc Cường", Address = "Thôn 8 xã Earal huyện Eahleo tỉnh Đak Lak", Phone = "0377077630"},
+                new CustomerModel{ Id = 6, Name = "Nguyễn Quốc Cường", Address = "Thôn 8 xã Earal huyện Eahleo tỉnh Đak Lak", Phone = "0377077630"},
+            };
+            return customer;
+        }
+    }
+}
