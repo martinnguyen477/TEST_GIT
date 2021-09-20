6bd8b257fa295cc91f256307d145946f55b08aae
nqcuong720 <nqcuong720@gmail.com>
2021-09-21 01:13:27 +0700

    dev update

--- CRUD_KENDOUI/Controllers/HomeController.cs
+++ CRUD_KENDOUI/Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace CRUD_KENDOUI.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        //dev update
     }
 }
