b7576254a5557e262daabf59a5d7e4070e878835
CuongNQ43 <CuongNQ43@fpt.com.vn>
2021-10-09 11:30:58 +0700

    okok

--- CRUD_KENDOUI/Controllers/CustomerController.cs
+++ CRUD_KENDOUI/Controllers/CustomerController.cs
@@ -19,6 +19,7 @@ namespace CRUD_KENDOUI.Controllers
             return new JsonResult(result);
             ///casgasgasga force checkout/
             //hhh/h/h/h/h/h/h/
+            ///Duy
 
         }
     }
